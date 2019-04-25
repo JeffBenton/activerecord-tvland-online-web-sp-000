920d6834ca31f2f87bca9c055900597359a57ba2
Jeff Benton <sikletz@gmail.com>
2019-04-25 19:05:11 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_day_season_to_show.rb
+++ db/migrate/009_add_day_season_to_show.rb
@@ -1,4 +1,4 @@
-class AddDaySeasonToShow < ActiveRecord::Migration [5.2]
+class AddDaySeasonToShow < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :integer
